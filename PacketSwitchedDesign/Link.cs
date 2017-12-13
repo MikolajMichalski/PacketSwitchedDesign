4589704083c6f74bf4b070404f49b60404a4a38f
MikolajMichalski <miki.michalski@gmail.com>
2017-12-13 14:28:13 +0100

    added missing files

---
+++ PacketSwitchedDesign/Link.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketSwitchedDesign
+{
+    public class Link
+    {
+        public string Index { get; set; }
+        public Router SourceRouter { get; set; }    
+        public Router DestRouter { get; set; }       
+        public float ThroughputEF { get; set; }
+        public float ThroughputAF { get; set; }
+        public float ThroughputBE { get; set; }
+        public int ThroughputOTN { get; set; }
+        public float LinkLength { get; set; }
+        public float B_EF { get; set; }
+        public float B_AF { get; set; }
+        public float B_BE { get; set; }
+        public float A_EF { get; set; }
+        public float A_AF { get; set; }
+        public float A_BE { get; set; }
+
+        public double B_EF1 { get; set; }
+        public double B_AF1 { get; set; }
+        public double B_BE1 { get; set; }
+        public Link(Router sourceRouter, Router destRouter,  float linkLength)
+        {
+            this.SourceRouter = sourceRouter;
+            this.DestRouter = destRouter;
+            this.LinkLength = linkLength;
+            this.Index = sourceRouter.Number.ToString() + destRouter.Number.ToString();
+            this.ThroughputEF = 0;
+            this.ThroughputAF = 0;
+            this.ThroughputBE = 0;
+            this.B_EF = 0;
+            this.B_AF = 0;
+            this.B_BE = 0;
+            this.A_EF = 0;
+            this.A_AF = 0;
+            this.A_BE = 0;
+            this.B_EF1 = 0;
+            this.B_AF1 = 0;
+            this.B_BE1 = 0;
+        }
+    }
+}
---
+++ PacketSwitchedDesign/Pages/AddRouterPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PacketSwitchedDesign.Pages
+{
+    /// <summary>
+    /// Interaction logic for AddRouterPage.xaml
+    /// </summary>
+    public partial class AddRouterPage : Page
+    {
+
+        public AddRouterPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CreateRouter(object sender, RoutedEventArgs e)
+        {
+
+            if (RouterTypeSelection.SelectedItem != null && !String.IsNullOrEmpty(NodeNumber.Text))
+            {
+                if (RouterTypeSelection.Text.Equals("Brzegowy"))
+                {
+                    if (MainPage.network.Routers.Count(x => x.Number == int.Parse(NodeNumber.Text) && x.Type == "Brzegowy") == 0)
+                    {
+                        if (MainPage.network.Routers.Count(x => x.Number == int.Parse(NodeNumber.Text)) == 0)
+                        {
+                            var router = new Router(int.Parse(NodeNumber.Text), RouterTypeSelection.Text);
+                            MainPage.network.Routers.Add(router);
+                           // MessageBox.Show("Dodano rute brzegowy");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ruter o tym numerze już istnieje");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Taki ruter już istnieje");
+                    }
+                }
+                else if (RouterTypeSelection.Text.Equals("Rdzeniowy"))
+                {
+                    if (MainPage.network.Routers.Count(x => x.Number == int.Parse(NodeNumber.Text) && x.Type == "Rdzeniowy") == 0)
+                   { 
+                        if (MainPage.network.Routers.Count(x => x.Number == int.Parse(NodeNumber.Text)) == 0)
+                        {
+                            var router = new Router(int.Parse(NodeNumber.Text), RouterTypeSelection.Text);
+                            MainPage.network.Routers.Add(router);
+                          //  MessageBox.Show("Dodano ruter rdzeniowy");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ruter o tym numerze już istnieje");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Taki ruter już istnieje");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Podaj wszystkie wymagane dane");
+            }
+        
+            MainPage.createNetworkPage.NumberOfNodes.DataContext = MainPage.network.Routers.Count.ToString();
+            MainPage.createNetworkPage.NumberOfEdgeNodes.DataContext = MainPage.network.Routers.Count(x => x.Type == "Brzegowy");
+            MainPage.createNetworkPage.NumberOfCoreNodes.DataContext = MainPage.network.Routers.Count(x => x.Type == "Rdzeniowy");
+        }
+    }
+}
